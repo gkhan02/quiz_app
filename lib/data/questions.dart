b02c1e55ff178c6ddaf6ba11bacc0c4076bc4ef7
gulnuma khan <gkhan02@nyit.edu>
2025-02-25 12:03:00 -0500

    Created Question Model and Questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,50 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+
+QuizQuestion(
+  'What color is the wall in the classroom?', 
+  [
+    'Grey' //Correct Answer
+    'Green'
+    'Pink'
+    'Blue'
+  ]
+),
+QuizQuestion(
+  'Do we open the window in the classroom?', 
+  [
+    'Yes' //Correct Answer
+    'No'
+    'Maybe'
+    'So'
+  ]
+),
+QuizQuestion(
+  'Does Professor Salayka enjoy using Apple Products?', 
+  [
+    'No' //Correct Answer
+    'Sometimes'
+    'Yes'
+    'All the time!'
+  ]
+),
+QuizQuestion(
+  'Why does NYIT have bad food?', 
+  [
+    'They are cheap' //Correct Answer
+    'They dont have the time'
+    'They dont have the budget'
+    'You dont pay enough'
+  ]
+),
+QuizQuestion(
+  'What is the first step in making GitHub commits', 
+  [
+    'Stage all changes' //Correct Answer
+    'Write a message'
+    'Commit'
+    'Commit and push'
+  ]
+),
+];
---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,8 @@
+class QuizQuestion {
+  
+const QuizQuestion(this.text, this.answers); //constructor
+
+  final String text; //Question text
+  final List<String> answers;
+  
+}
